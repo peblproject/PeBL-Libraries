b630ac6c1375a3d226c26cdf2d77339c1cf2e6b8
Mile Divovic <md2950@nyu.edu>
2019-03-14 12:00:21 -0700

    bump indexeddb version

--- src/storage.ts
+++ src/storage.ts
@@ -14,7 +14,7 @@ export class IndexedDBStorageAdapter implements StorageAdapter {
     private invocationQueue: Function[] = [];
 
     constructor(callback: () => void) {
-        let request = window.indexedDB.open("pebl", 18);
+        let request = window.indexedDB.open("pebl", 19);
         let self: IndexedDBStorageAdapter = this;
 
         request.onupgradeneeded = function() {
